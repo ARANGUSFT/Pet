9ac601c592dab67c123f17f974efff82667ab124
arnol agudelo <arnolagudelo7>
2023-05-09 18:53:54 +0200

    Avances

--- Pet/Public/Js/ElejirEstilo.js
+++ Pet/Public/Js/ElejirEstilo.js
@@ -1,7 +1,7 @@
-const select = document.getElementById("seleccion");
-const imagen = document.getElementById("imagen");
+const select = document.getElementById("select-imagen");
+const imagen = document.getElementById("imagen-seleccionada");
 
-select.addEventListener("change", function(){
+select.addEventListener("change", function () {
     const valor = select.value;
     imagen.src = "{% static '" + valor + "'%}";
     imagen.alt = select.options[select.selectedIndex].text;
