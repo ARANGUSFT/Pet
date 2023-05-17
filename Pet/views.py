ef8e03d7434e7e9ed14e2dee7de2206992a3caad
arnol agudelo <arnolagudelo7>
2023-05-17 15:54:20 +0200

    x

--- Pet/views.py
+++ Pet/views.py
@@ -332,12 +332,6 @@ def InsertarDueño(request):
 #region Placa
 def InsertarEstiloPlaca(request):
    return render(request, 'MisMascotas/EstiloPlaca.html')
-   """  if request.method == "POST":
-   return render(request, 'MisMascotas/EstiloPlaca.html')
-   """  if request.method == "POST":
-        with connection.cursor() as cursor:
-            cursor.callproc('InsertarEstilos', [request.POST.get('Estilo_Placa_C'), request.POST.get('Color_Placa_C')])
-        return redirect('/MisMascotas/Botones') """
     
 
 #endregion
